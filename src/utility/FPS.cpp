8de27cc60c98c34fd8245df7d8c3fedacde70ed5
chaosink <linhsu0723@qq.com>
2017-12-31 17:23:12 +0800

    FPS n_frame_

--- src/utility/FPS.cpp
+++ src/utility/FPS.cpp
@@ -3,8 +3,8 @@
 #include <cstdio>
 
 float FPS::Update(double time) {
-	if(c_frame_++ % 10 == 0) {
-		fps_ = 10 / (time - time_);
+	if(c_frame_++ % n_frame_ == 0) {
+		fps_ = n_frame_ / (time - time_);
 		time_ = time;
 		printf("\rFPS: %f", fps_);
 		fflush(stdout);
--- src/utility/FPS.hpp
+++ src/utility/FPS.hpp
@@ -2,6 +2,7 @@
 
 class FPS {
 	int c_frame_ = 0;
+	int n_frame_ = 10;
 	double time_ = 0;
 	float fps_ = 0;
 public:
